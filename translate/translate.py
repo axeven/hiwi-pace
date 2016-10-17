47902045a251b67aa26b599d1d0383410a5baab6
Debjit <deb_4u22@yahoo.com>
2016-10-17 13:11:01 +0200

    read_file

--- translate/translate.py
+++ translate/translate.py
@@ -17,52 +17,53 @@ def read_uai_file(uaifile):
         lines = file.readlines()
         file.readlines()
     graph = {}
-    for line in lines:
-        # assumes the .gr file is valid
-        if line[0] == 'c':
-            continue
-        if line[0] == 'p':
-            # need to read vertices and edges count?
-            continue
-        v = line.split(' ')
-        a = int(v[0])
-        b = int(v[1])
-        if a not in graph:
-            graph[a] = [b]
-        else:
-            graph[a].append(b)
-        if b not in graph:
-            graph[b] = [a]
-        else:
-            graph[b].append(a)
+    index=0
+    i=0
+    count_clique=0
+    count_clique =int(lines[3])	
+    for index in range(4,4+count_clique):
+        #print (lines[index])
+        
+        v=lines[index].split(' ')
+        w=len(v)
+        v[-1] = v[-1].strip()
+        v[-1] = v[-1].strip(' ')
+        w=len(v)
+        #print ("w",w) 
+        if (w>3) :
+          graph[i]=(v[1:w])
+          i=i+1
+    print (graph)    
+        
     return graph
 
 
 def translate(graph):
     # return output graph
     # [...]
-    Globalqueue = [center]  ##Globalqueue will store all the vertices of the small graph
-    q = []
-    q[0] = [center]
-    q[1] = graph[center]  ##storing all  the neighbours
-    r = 1
-    return []
+    ng = {}
+    #for index in range(0,len(graph)):
+     #   ng[index+1] = graph[Globalqueue[index]]
+    ng = graph
+    return []  
+    
 
 
 def print_gr_file(graph):
     # Print graph to (already opened) outfile stream
     # [...]
-    print('Not yet implemented')
+     
+  print('Not yet implemented')
 
 
-def main():
+def main():	
     parser = argparse.ArgumentParser()
     parser.add_argument("uaifile", help=".uai file containing the input graph")
     args = parser.parse_args()
 
     graph = read_uai_file(args.uaifile)
     grgraph = translate(graph)
-    print_gr_file(grgraph)
+    print_gr_file(graph)
 
 if __name__ == '__main__':
     main()
