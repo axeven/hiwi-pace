ce7e04ed9f4a166cece59516d25cd63570bf2072
Muhammad Lathif Pambudi <axe7.mail@gmail.com>
2016-11-02 13:16:05 +0100

    fixing bug for the case when there is a duplicate or self loop edges

--- cleaner/make_clean.py
+++ cleaner/make_clean.py
@@ -43,7 +43,7 @@ class Component:
         return str(self.graph) + '\n' + str(self.members) + '\n' + str(self.count)
 
 
-def read_gr_file(grfile):
+def read_gr_file_and_clean(grfile):
     # Read from grfile or stdin and store in a suitable data structure
     if grfile is None:
         file = sys.stdin
@@ -58,16 +58,26 @@ def read_gr_file(grfile):
             # need to read vertice and edges count?
             temp = line.split(' ')
             vcount = int(temp[2])
-            ecount = int(temp[3])
-            graph = [[] for i in range(vcount + 1)]
+            # ecount = int(temp[3]) we do not read vertice count because we are removing
+            # self loop and duplicates
+            graph = [set() for i in range(vcount + 1)]
             continue
         v = line.split(' ')
         a = int(v[0])
         b = int(v[1])
-        graph[a].append(b)
-        graph[b].append(a)
+        if a != b:
+            graph[a].add(b)
+            graph[b].add(a)
+        if a > vcount:
+            print(a, 'larger than', vcount)
+        if b > vcount:
+            print(b, 'larger than', vcount)
     file.close()
-    return graph, vcount, ecount
+    ecount = 0
+    for i in range(len(graph)):
+        graph[i] = list(graph[i])
+        ecount += len(graph[i])
+    return graph, len(graph)-1, int(ecount / 2)
 
 
 def print_gr_file(graph, ecount, header=None):
@@ -83,6 +93,7 @@ def print_gr_file(graph, ecount, header=None):
 def remove_edges_on_degree_one_vertices(graph, V, E):
     # print('Removing degree one ...')
     queue = []
+    new_v = V
     added_to_queue = bitarray(V + 1)  # index 0 is a padding
     added_to_queue.setall(False)
     for i in range(1, V + 1):
@@ -99,22 +110,23 @@ def remove_edges_on_degree_one_vertices(graph, V, E):
                     next_queue.append(v)
                     added_to_queue[v] = True
             new_e -= len(graph[pop])
+            new_v -= 1
             graph[pop] = []
         queue = next_queue
-    return graph, V, new_e
+    return graph, new_v, new_e
 
 
-def get_largest_component(graph, vcount):
+def get_largest_component(graph):
     # print('Getting largest component ...')
-    added_to_queue = bitarray(vcount + 1)
+    container_size = len(graph)
+    added_to_queue = bitarray(container_size)
     added_to_queue.setall(False)
     added_to_queue_count = 0
-    max_component = Component(vcount)
-    for u in range(1, vcount + 1):
+    max_component = Component(container_size)
+    for u in range(1, container_size):
         if not added_to_queue[u]:
             queue = [u]
-            next_queue = []
-            component = Component(vcount)
+            component = Component(container_size)
             component.add_vertice(u)
             added_to_queue[u] = True
             added_to_queue_count += 1
@@ -128,15 +140,50 @@ def get_largest_component(graph, vcount):
                             added_to_queue_count += 1
                             component.add_vertice(v)
                     component[q] = graph[q]
+                    for w in graph[q]:
+                        if w == q:
+                            print('something went wrong')
                 queue = next_queue
             if component > max_component:
                 max_component = component
-                if max_component.count > vcount / 2:
+                if max_component.count > container_size / 2:
                     # if more than half of vertices is in one component
                     # then that component is definitely the largest one
                     break
     return max_component.graph, max_component.members, max_component.count, int(
         sum(len(vs) for vs in max_component) / 2)
+
+
+def assert_valid_graph(graph, V, E):
+    # assumes if node does not have edges then those node does not exists
+    node_exists = bitarray(len(graph))
+    node_exists.setall(False)
+    should_exists = bitarray(len(graph))
+    should_exists.setall(False)
+    v_count = 0
+    e_count = 0
+    for u in range(len(graph)):
+        if len(graph[u]) > 0:
+            node_exists[u] = True
+            v_count += 1
+            e_count += len(graph[u])
+            for v in graph[u]:
+                if v == 0:
+                    print(u, 'is referencing 0')
+                if u == v:
+                    print(u, 'is referencing itself')
+                should_exists[v] = True
+    e_count = int(e_count / 2)
+    if v_count != V:
+        print('V count does not match: {:d} and {:d}'.format(V, v_count))
+    if e_count != E:
+        print('E count does not match: {:d} and {:d}'.format(E, e_count))
+
+    for i in range(len(node_exists)):
+        if not node_exists[i] and should_exists[i]:
+            print('Error: {:d} does not have reference'.format(i))
+        if node_exists[i] and not should_exists[i]:
+            print('Error: {:d} is not referenced'.format(i))
 
 
 def relabel_graph(graph, selected_vertices):
@@ -164,12 +211,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("grfile", help=".gr file containing the input graph", nargs='?')
     args = parser.parse_args()
-    graph, V, E = read_gr_file(args.grfile)
+    graph, V, E = read_gr_file_and_clean(args.grfile)
     graph, V, E = remove_edges_on_degree_one_vertices(graph, V, E)
-    new_graph, Vs, V_, E_ = get_largest_component(graph, V)
+    graph, Vs, V_, E_ = get_largest_component(graph)
     if V != V_:
-        new_graph = relabel_graph(new_graph, Vs)
-    print_gr_file(new_graph, E_)
+        graph = relabel_graph(graph, Vs)
+        print_gr_file(graph, E_)
 
 
 if __name__ == '__main__':
